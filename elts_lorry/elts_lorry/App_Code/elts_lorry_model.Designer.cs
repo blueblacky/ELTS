d53308724cb92d5ae8bdec31ef774f134e17de13
blueblacky <blue.blacky@live.com>
2015-01-02 16:35:34 +0530

    ELTS

--- elts_lorry/elts_lorry/App_Code/elts_lorry_model.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'D:\Sathya\Programs 2012\elts_lorry\App_Code\elts_lorry_model.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
